85f0263542f2541d130f380a6d765f94bf1f83ad
MuhammadSaddique <97628071+MuhammadSaddique@users.noreply.github.com>
2025-06-02 01:47:36 +0500

    todo game

--- app.js
+++ app.js
@@ -594,16 +594,16 @@
 //    Event listner
 
 
-let p = document.querySelector("p");
+// let p = document.querySelector("p");
 
-p.addEventListener("click",function(){
-    console.log("Paragraph is clicked");
-})
+// p.addEventListener("click",function(){
+//     console.log("Paragraph is clicked");
+// })
 
-let box = document.querySelector(".box");
-box.addEventListener("mouseenter",function(){
-    console.log("mouse is inside the box!");
+// let box = document.querySelector(".box");
+// box.addEventListener("mouseenter",function(){
+//     console.log("mouse is inside the box!");
     
-})
+// })
 
 
